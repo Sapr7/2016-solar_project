c5bf07a2e7519f91e1b55bcddc84d3cd9878deed
VandyshGe <vandyshev.gk@phystech.edu>
2020-11-13 01:53:06 +0300

    исправил типы

--- solar_model.py
+++ solar_model.py
@@ -19,7 +19,7 @@ def calculate_force(body, space_objects):
         if body == obj:
             continue  # тело не действует гравитационной силой на само себя!
         r = ((body.x - obj.x)**2 + (body.y - obj.y)**2)**0.5
-        F = gravitational_constant * body.m * obj.m / r**2
+        F = - gravitational_constant * body.m * obj.m / r**2
         body.Fx += F * (body.x - obj.x) / r
         body.Fy += F * (body.y - obj.y) / r
 
